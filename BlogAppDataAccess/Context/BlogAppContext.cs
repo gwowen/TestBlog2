a41976dd527677f752b1fc34e9a85b945096233b
Gareth Owen <gareth.w.owen@gmail.com>
2015-10-06 00:29:15 +0100

    Began work on database context

---
+++ BlogAppDataAccess/Context/BlogAppContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogAppDataAccess.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class BlogAppContext : DbContext
+    {
+
+    }
+}
