68d4fc0d973b7566fb22d973a61b41458696a1ee
Gareth Owen <gareth.w.owen@gmail.com>
2015-10-07 00:19:05 +0100

    Added Repository interface and CategoryRepository

---
+++ BlogAppDataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using BlogAppDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppDataAccess.Repositories
+{
+    class CategoryRepository : IRepository<Category>
+    {
+        BlogAppContext m_Context = null;
+
+        public CategoryRepository(BlogAppContext context)
+        {
+            m_Context = context;
+        }
+
+        public IEnumerable<Category> GetAll(System.Linq.Expressions.Expression<Func<Category, bool>> predicate = null)
+        {
+            return m_Context.Categories.Where(predicate);
+        }
+
+        public Category Get(System.Linq.Expressions.Expression<Func<Category, bool>> predicate)
+        {
+            return m_Context.Categories.SingleOrDefault(predicate);
+        }
+
+        public void Add(Category entity)
+        {
+            m_Context.Categories.Add(entity);
+        }
+
+        public void Update(Category entity)
+        {
+            m_Context.Categories.Attach(entity);
+            ((IObjectContextAdapter)m_Context).ObjectContext.ObjectStateManager.ChangeObjectState(entity, EntityState.Modified);
+        }
+
+        public void Delete(Category entity)
+        {
+            m_Context.Categories.Remove(entity);
+        }
+
+        public int Count()
+        {
+            return m_Context.Categories.Count();
+        }
+    }
+}
---
+++ BlogAppDataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppDataAccess
+{
+    public interface IRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate = null);
+        T Get(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int count();
+    }
+}
